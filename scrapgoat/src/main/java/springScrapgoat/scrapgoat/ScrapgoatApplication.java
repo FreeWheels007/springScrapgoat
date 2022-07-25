8e8484b83577f0d3444d11cfe76e48efbc4f0c5f
Bernard Ross <bernardross0763@gmail.com>
2022-07-25 10:25:46 -0400

    Spring boot init and comment out jpa

---
+++ scrapgoat/src/main/java/springScrapgoat/scrapgoat/ScrapgoatApplication.java
@@ -0,0 +1,13 @@
+package springScrapgoat.scrapgoat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapgoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapgoatApplication.class, args);
+	}
+
+}
---
+++ scrapgoat/src/test/java/springScrapgoat/scrapgoat/ScrapgoatApplicationTests.java
@@ -0,0 +1,13 @@
+package springScrapgoat.scrapgoat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapgoatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
